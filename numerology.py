933943954e49a7549cb3ec9eab22ab3a2978da27
repl-unix <sigdananderson@gmail.com>
2021-10-03 20:41:10 +0000

    ok filepath start of the program

--- numerology.py
+++ numerology.py
@@ -1,19 +1,21 @@
 import parsing as prs
 import utils as ut
 
-file_input_words = open("./config/input_file.txt", "r")
-file_n_to_find = open("./config/numero_da_trovare.txt", "r")
-file_numerology_type = open("./config/numerology_type.txt", "r")
 filepath_ita_dictionary = "./dict/ita_dictionary.txt"
-file_ita_dictionary = open(filepath_ita_dictionary, "r")
-
 filepath_ita_dict_indexed = "./dict/ita_dic_indexed.txt"
+filepath_input_words = "./config/input_file.txt"
+filepath_n_to_find = "./config/numero_da_trovare.txt"
+filepath_numerology_type = "./config/numerology_type.txt"
+
+file_input_words = open(filepath_input_words, "r")
+file_n_to_find = open(filepath_n_to_find, "r")
+file_numerology_type = open(filepath_numerology_type, "r")
+
 
 n_tofind = file_n_to_find.read()
 input_words_lower = file_input_words.read()
 input_words = input_words_lower.upper()
 numbers_table = file_numerology_type.read()
-ita_dictionary = file_ita_dictionary.read()
 
 table = prs.parse_numerology_file(numbers_table)
 to_analyze = prs.parse_input_file(input_words)
@@ -36,8 +38,6 @@ karma = ut.reduce_numbers_up_to_1000(data_sum)
 
 print("\n[", signature, "] firma", signature_integral, "ridotta", signature_reduced)
 print ("[", signature, "]", "è un", str(day_reduced) + "K" + str(karma), "con firma", signature_reduced, "\n")
-
-#print(n_tofind)
 
 #da aggiungere tipo 2K8 significa che...
 #da aggiungere la lista di parole prese da un file e il numero desiderato...
