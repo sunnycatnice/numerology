e2aab1b82261a65f6378d9ba6c3937dcb4fff70f
daniele-MacOS <sio2guanoeleo@gmail.com>
2022-10-23 22:16:43 +0200

    req scripts ok

---
+++ scripts/generate_requirements.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+PYVERSION=3.10
+pip${PYVERSION} freeze > requirements.txt
--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,11 +1,4 @@
 #!/bin/zsh
 
-# This script generates a requirements.txt file from the current environment
-# and installs the packages in the requirements.txt file.
-PYVERSION=3.10.6
-
-pip${PYVERSION} freeze > requirements.txt
-
-# Installing packages from requirements.txt
 pip${PYVERSION} install -r requirements.txt
 
