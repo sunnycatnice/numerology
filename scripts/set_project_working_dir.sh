3aec42ee27cdd7523a7b90fcafb0509e8107ff7e
daniele-MacOS <sio2guanoeleo@gmail.com>
2022-10-23 21:56:46 +0200

    Script finished

--- scripts/set_project_working_dir.sh
+++ scripts/set_project_working_dir.sh
@@ -6,6 +6,10 @@ cd ..
 PYTHON_PROJECT_FOLDER="PYTHON_PROJECT_FOLDER"
 PYTHON_PROJECT_PATH=$PWD
 
+#if the first argument is "force", set variable FORCE to true
+if [ "$1" = "force" ]; then
+    FORCE=true
+fi
 
 function backup_old_path()
 {
@@ -14,7 +18,7 @@ function backup_old_path()
     then
         touch $HOME/.old_paths
     fi
-    # Check if the key is already in the file
+    # Check if the path is already in the file
     if ! grep -q $PYTHON_PROJECT_PATH $HOME/.old_paths
     then
         # If not, add it to the file
@@ -28,23 +32,30 @@ function add_proj_dir_path ()
     if ! grep -q $PYTHON_PROJECT_FOLDER $1; then
         echo "$PYTHON_PROJECT_FOLDER path not found in $1, adding it...\n"
         echo "export $PYTHON_PROJECT_FOLDER='$PYTHON_PROJECT_PATH'" >> $1
-        #echo with green color key added!
-        echo -e "\e[32mKey added!\e[0m\n"
+        #echo with green color path added!
+        echo -e "\e[32mPath added!\e[0m\n"
     else
-        echo "$PYTHON_PROJECT_FOLDER path found in $1, make sure to check if it's a functioning key!\n"
-        #ask if the user wants to change the key
-        echo -n "Do you want to change the key? [y/n]: "
-        read CHANGE
-        if [ $CHANGE = "y" ]
-        then
+        echo "$PYTHON_PROJECT_FOLDER path found in $1, make sure to check if it's a functioning path!\n"
+        #if force is true, overwrite the path
+        if [ "$FORCE" = true ]; then
             backup_old_path
             sed -i.old '/PYTHON_PROJECT_FOLDER/d' $1
             echo "export $PYTHON_PROJECT_FOLDER='$PYTHON_PROJECT_PATH'" >> $1 
-            #echo with green color key changed!
-            echo -e "\e[32mKey changed!\e[0m\n"
+            #echo with green color path changed!
+            echo -e "\e[32mPath changed!\e[0m\n"
         else
-            #print with yellow color key not changed
-            echo -e "\e[33mKey not changed\e[0m\n"
+            echo -e "Do you want to overwrite the $PYTHON_PROJECT_FOLDER path in $1? [y/n]"
+            read -r answer
+            if [ "$answer" = "y" ]; then
+                backup_old_path
+                sed -i.old '/PYTHON_PROJECT_FOLDER/d' $1
+                echo "export $PYTHON_PROJECT_FOLDER='$PYTHON_PROJECT_PATH'" >> $1 
+                #echo with green color path changed!
+                echo -e "\e[32mPath changed!\e[0m\n"
+            else
+                #print with yellow color path not changed
+                echo -e "\e[33mPath not changed\e[0m\n"
+            fi
         fi
     fi
 }
@@ -54,4 +65,4 @@ add_proj_dir_path $HOME/.bashrc
 
 cd scripts
 
-source $HOME/.zshrc; source $HOME/.bashrc; exec zsh
+source $HOME/.zshrc; source $HOME/.bashrc;
--- scripts/start_virtual_env.sh
+++ scripts/start_virtual_env.sh
@@ -12,18 +12,24 @@ DEFAULT_COLOR='\033[0m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 
+#do not ask yes/no questions and update the path of the project
+/bin/zsh ./set_project_working_dir.sh force
+
+# Create a virtual environment
 if [ -d .venv ]; then
     echo "${GREEN}Virtual environment already exists, skipping creation${RESET_COLOR}"
 else
-    echo "${GREEN}Creating virtual environment..."
-    python${PYVERSION} -m venv $PYTHON_PROJECT_FOLDER/.venv
+    echo -n "${GREEN}Creating virtual environment with "
     echo "python${PYVERSION} -m venv .venv"
+    python${PYVERSION} -m venv $PYTHON_PROJECT_FOLDER/.venv
     echo "${BOLD_GREEN}Virtual environment created${RESET_COLOR}"
 fi
 
-chmod 755 $PWD/.venv/bin/activate
-$PWD/.venv/bin/activate
-clear
+# chmod 755 $PYTHON_PROJECT_FOLDER/.venv/bin/activate
+# $PYTHON_PROJECT_FOLDER/.venv/bin/activate
 
-echo "${BOLD_GREEN}✓ You are now in a python${PYVERSION} virtual environment!\n  ${GREEN}Type exit or ctrl+d to exit${RESET_COLOR}"
+echo "${BOLD_GREEN}✓ You are now in a python${PYVERSION} virtual environment!" 
+echo "  ${GREEN}Type ${BOLD_GREEN}exit${GREEN} or ${BOLD_GREEN}ctrl+d${GREEN} ${RED}to exit${RESET_COLOR}"
+echo "  ${GREEN}If you find yourself in VScode you may be by default in a python virtual env.${RESET_COLOR}"
+echo "  ${GREEN}If you want to exit the virtual env, type ${YELLOW}deactivate${GREEN} in the terminal${RESET_COLOR}"
 exec $SHELL
